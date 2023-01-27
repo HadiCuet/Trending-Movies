5efbe4ef905c856d7e99d832222d3befe252d300
Abdullah Al Hadi <hadi.cuet@gmail.com>
2023-01-27 10:49:54 +0600

    Project created
    Signed-off-by: Abdullah Al Hadi <hadi.cuet@gmail.com>

---
+++ Trending Movies/Trending Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trending Movies
+//
+//  Created by Abdullah Al Hadi on 27/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
