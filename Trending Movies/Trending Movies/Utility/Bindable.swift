4f1ba04c00eae3839610ce8ea4b3740b1229cf88
Abdullah Al Hadi <hadi.cuet@gmail.com>
2023-01-27 14:42:20 +0600

    View model added for home screen
    Signed-off-by: Abdullah Al Hadi <hadi.cuet@gmail.com>

---
+++ Trending Movies/Trending Movies/Utility/Bindable.swift
@@ -0,0 +1,25 @@
+//
+//  Bindable.swift
+//  Trending Movies
+//
+//  Created by Abdullah Al Hadi on 27/1/23.
+//
+
+class Bindable<T> {
+    typealias Listener = ((T) -> Void)
+    var listener: Listener?
+
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    init(_ v: T) {
+        self.value = v
+    }
+
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+}
